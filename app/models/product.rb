007db367efeb0dfe09cedfe949d4b3b1ecad8bc5
Luke Jones <luke@freerunningtechnologies.com>
2016-05-08 11:40:39 -0400

    Add paperclip attachment to products
    Products will soon use paperclip attachements instead of an implicit
    URL. This commit adds this attachment to the product model and some
    validations and formats for the uploaded images.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,13 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
+  has_attached_file :product_image, styles: { slider: "542x668#" }
 
   validates :name, presence: true
   validates :SKU, presence: true
   validates :image_path, presence: true
+  validates_attachment :product_image, presence: true,
+    content_type: { content_type: "image/jpeg" },
+    size: { less_than: 500.kilobytes }
 
   scope :featured, -> { where(featured: true) }
 
