d54a2c06829b3e6c9bde025e714383e3ed27e439
Luke Jones <luke@freerunningtechnologies.com>
2016-05-08 17:04:20 -0400

    Add new admin product form
    Added page for new admin products. Admins can now add new products.

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -8,9 +8,21 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def new
+    @product = Product.new
+    @categories = Category.all
   end
 
   def create
+    @product = Product.new(product_params)
+    @categories = Category.all
+    @product.categories = Category.find(params[:product][:category_ids]) unless params[:product][:category_ids].nil?
+    @product.image_path = "products/lol.jpg"
+
+    if @product.save
+      redirect_to admin_product_url(@product)
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -20,5 +32,11 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :SKU, :price, :featured, :product_image)
   end
 end
