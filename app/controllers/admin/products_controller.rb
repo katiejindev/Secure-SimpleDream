70db99f0b18cb87c5a67e1ac07ed8b1f3ffb157f
Luke Jones <luke@freerunningtechnologies.com>
2016-05-08 17:34:46 -0400

    Add admin products edit page
    You can now edit existing products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -26,9 +26,32 @@ class Admin::ProductsController < Admin::BaseController
   end
 
   def edit
+    @product = Product.find(params[:id])
+    @categories = Category.all
   end
 
   def update
+    @product = Product.find(params[:id])
+    @product.attributes = product_params
+    @categories = Category.all
+    @product.categories = Category.find(params[:product][:category_ids]) unless params[:product][:category_ids].nil?
+    @product.image_path = "products/lol.jpg"
+
+    if params[:product][:category_ids]
+      removed_categories = @product.categories.where("id NOT IN (?)", params[:product][:category_ids])
+
+      removed_categories.each do |category|
+        @product.categories.delete(category)
+      end
+    else
+      @product.categories.clear
+    end
+
+    if @product.save
+      redirect_to admin_product_url(@product)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
